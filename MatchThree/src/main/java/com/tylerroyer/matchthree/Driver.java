cb5abddd6a4372d3255f346d9d66d51d4f6f24f9
hades948 <hades948@gmail.com>
2019-12-29 10:41:40 -0500

    Updated to Molasses 0.1.5

--- MatchThree/src/main/java/com/tylerroyer/matchthree/Driver.java
+++ MatchThree/src/main/java/com/tylerroyer/matchthree/Driver.java
@@ -4,7 +4,10 @@ import com.tylerroyer.molasses.*;
 
 public class Driver {
     public static void main( String[] args ) {
-        Resources.init(1920.0, 1080.0);
-        Game.init(867, 910, "Krystal's Quest", new GridScreen());
+        Config.windowWidth = 867;   Config.windowHeight = 910;
+        Config.windowTitle = "Krystal's Quest";
+        Config.firstScreen = new GridScreen();
+        
+        Game.start();
     }
 }
